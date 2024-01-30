72320131c701d83314123d68c32813bf1ebaddcb
Toriq847 <154895775+Toriq847@users.noreply.github.com>
2024-01-30 17:35:24 +0700

    Meme.py

---
+++ Meme.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from projek import gen_pass
+import random, os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def  password(ctx):
+    await ctx.send('BERIKUT PASSWORD UNTUKMU:')
+    await ctx.send(gen_pass())
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def repeat(ctx, times = 3, content='INI SEBUAH PERULANGAN'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def meme(ctx):
+    gambar = random.choice(os.listdir('images'))
+    with open(f'images/{gambar}', 'rb') as f:
+        pic = discord.File(f)
+
+    await ctx.send(file=pic)
